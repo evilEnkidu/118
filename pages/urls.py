3162977f0dab9c74a5ceb0b7a25636c4025446df
evilEnkidu <maganae@uabc.edu.mx>
2024-12-20 11:03:28 -0800

    Landing page added

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, about
 # ahhah
 urlpatterns = [
       path("", home, name="root"),
       path("home/", home, name="home"),
+      path("about/", about, name="about"),
 ]
